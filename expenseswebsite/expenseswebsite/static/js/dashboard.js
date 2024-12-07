2d5437c84c53c78f58a37d7b32770c95ab91e038
Shripatil200 <patilshriram200@gmail.com>
2024-12-07 08:08:25 +0530

    change

--- expenseswebsite/expenseswebsite/static/js/dashboard.js
+++ expenseswebsite/expenseswebsite/static/js/dashboard.js
@@ -1,49 +1,65 @@
-/* globals Chart:false */
-
 (() => {
-    'use strict'
-  
-    // Graphs
-    const ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
-    const myChart = new Chart(ctx, {
+  'use strict'
+
+  // Graphs
+  const ctx = document.getElementById('myChart');
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
       type: 'line',
       data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: [
-            15339,
-            21345,
-            18483,
-            24003,
-            23489,
-            24092,
-            12034
+          labels: [
+              'January',
+              'February',
+              'March',
+              'April',
+              'May',
+              'June',
+              'July',
+              'August',
+              'September',
+              'October',
+              'November',
+              'December'
           ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
+          datasets: [
+              {
+                  label: 'Income',
+                  data: [
+                      50000, 45000, 48000, 52000, 55000, 60000, 58000, 57000, 59000, 62000, 61000, 63000
+                  ],
+                  lineTension: 0,
+                  backgroundColor: 'transparent',
+                  borderColor: '#007bff',
+                  borderWidth: 4,
+                  pointBackgroundColor: '#007bff'
+              },
+              {
+                  label: 'Expenses',
+                  data: [
+                      40000, 35000, 37000, 40000, 42000, 45000, 43000, 44000, 46000, 48000, 47000, 50000
+                  ],
+                  lineTension: 0,
+                  backgroundColor: 'transparent',
+                  borderColor: '#ff073a',
+                  borderWidth: 4,
+                  pointBackgroundColor: '#ff073a'
+              }
+          ]
       },
       options: {
-        plugins: {
-          legend: {
-            display: false
+          plugins: {
+              legend: {
+                  display: true
+              },
+              tooltip: {
+                  boxPadding: 3
+              }
           },
-          tooltip: {
-            boxPadding: 3
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
           }
-        }
       }
-    })
-  })()
+  });
+})();
